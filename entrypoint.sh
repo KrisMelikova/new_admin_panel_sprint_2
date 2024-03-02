2f982bdee9ecdaf05a599f874e80892fda62569c
Kris Melikova <melikova@br-analytics.ru>
2024-03-02 11:31:13 +0300

    Changes arfer code review

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd movies_app
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --no-input;
+python manage.py createsuperuser --noinput || true;
+cd -
+
+cd sqlite_to_postgres
+python load_data.py
+cd -
+
+uwsgi --strict --ini uwsgi.ini
